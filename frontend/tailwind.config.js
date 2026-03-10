1333b78e888645bff5b5df61d8857349774e9f30
voyagerweb3 <voyager.web3@gmail.com>
2026-03-10 12:48:38 +0100

    feat: add frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        tg: {
+          bg: 'var(--tg-theme-bg-color)',
+          text: 'var(--tg-theme-text-color)',
+          hint: 'var(--tg-theme-hint-color)',
+          link: 'var(--tg-theme-link-color)',
+          button: 'var(--tg-theme-button-color)',
+          'button-text': 'var(--tg-theme-button-text-color)',
+          secondary: 'var(--tg-theme-secondary-bg-color)',
+        }
+      }
+    }
+  },
+  plugins: []
+}
